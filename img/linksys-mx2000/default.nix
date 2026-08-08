b0947b73215600ec0bc13b3c9e0b264234614c0f
Ujaan Das <ujaandas03@gmail.com>
2026-08-08 23:04:32 +0800

    rename to router specific name

--- img/linksys-mx2000/default.nix
+++ img/linksys-mx2000/default.nix
@@ -10,9 +10,10 @@ in
 openwrt-imagebuilder.lib.build {
   inherit pkgs;
 
-  release = "23.05.3";
-  target = "x86/64";
-  profile = "generic";
+  release = "25.12.5";
+  target = "qualcommax";
+  variant = "ipq50xx";
+  profile = "linksys_mx2000";
 
   # Packages baked into ROM
   packages = [
